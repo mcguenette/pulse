d73a24c9008687d8a1cfea5c28085608512c26e5
Robyn Waddell <retwaddell@shaw.ca>
2023-12-15 18:40:40 -0600

    most of home page

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,3 +1,11 @@
 'use strict';
 
 import { onEvent, select, selectAll } from './utils.js';
+
+$.ajax({
+    url: 'https://randomuser.me/api/',
+    dataType: 'json',
+    success: function(data) {
+      console.log(data);
+    }
+  });
