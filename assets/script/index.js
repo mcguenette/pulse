02ce57151555906d8c24f9b55b6b41337b755088
Robyn Waddell <retwaddell@shaw.ca>
2023-12-15 19:45:52 -0600

    api is working yayyyyy

--- assets/script/index.js
+++ assets/script/index.js
@@ -2,10 +2,30 @@
 
 import { onEvent, select, selectAll } from './utils.js';
 
-$.ajax({
-    url: 'https://randomuser.me/api/',
-    dataType: 'json',
-    success: function(data) {
-      console.log(data);
-    }
+
+
+// Fetch data from the Random User API
+fetch('https://randomuser.me/api/?nat=CA&results=10')
+.then(response => response.json())
+.then(data => {
+  const connectionsList = document.getElementById('connections-list');
+
+  data.results.forEach(user => {
+    const connectionItem = document.createElement('li');
+    connectionItem.classList.add('connection-item');
+
+    const connectionPic = document.createElement('img');
+    connectionPic.classList.add('connection-pic');
+    connectionPic.src = user.picture.thumbnail;
+    connectionPic.alt = 'Connection Profile Picture';
+
+    const connectionInfo = document.createElement('div');
+    connectionInfo.innerHTML = `<strong>${user.name.first} ${user.name.last}</strong><br>${user.location.city}`;
+
+    connectionItem.appendChild(connectionPic);
+    connectionItem.appendChild(connectionInfo);
+
+    connectionsList.appendChild(connectionItem);
   });
+})
+.catch(error => console.error('Error fetching data:', error));
